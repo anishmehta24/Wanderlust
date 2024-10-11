0a0ead03cd7d7c1f97069761411c7483aeaca337
Anish Mehta <mehtaanish111@gmail.com>
2024-10-11 10:05:46 +0530

    fixed bug'

--- models/listing.js
+++ models/listing.js
@@ -25,19 +25,6 @@ const listingSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User",
     },
-    geometry: {
-        type:{
-            type: String,
-            enum: ['Point'],
-            required: true,
-
-        },
-        coordinates: {
-            type: [Number],
-            required: true,
-        }
-
-    }
 });
 
 listingSchema.post("findOneAndDelete", async()=>{
